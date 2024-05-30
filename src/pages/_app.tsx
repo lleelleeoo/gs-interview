56ce696dd1f8a71da28a639f4f3f4c9b47dda4b6
Vsevolod Drobyshevskiy <vsbrute@gmail.com>
2024-05-30 19:57:27 +0300

    chore: clean-up a bit && setup color palette and main laypot

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,27 @@
 import "@/styles/globals.css";
+import "@/styles/theme.css";
 
+import { createTheme, ThemeProvider  } from "@mui/material";
 import type { AppProps } from "next/app";
+import { Poppins } from "next/font/google";
 
+const poppins = Poppins({ weight: "400", subsets: ["latin"] });
+const fontFamily = [
+    poppins.style.fontFamily,
+    "Arial",
+    "sans-serif",
+].join(",");
+
+const theme = createTheme({ typography: { fontFamily } });
+
+// Need to share layout between pages and add some global styles
+// https://nextjs.org/docs/pages/building-your-application/routing/custom-app
 function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <ThemeProvider theme={theme}>
+            <Component {...pageProps} />
+        </ThemeProvider>
+    );
 }
 
 export { App as default };
