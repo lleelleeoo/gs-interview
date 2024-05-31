8bd983c06d641b40e39828bcc04f991e038a3895
Vsevolod Drobyshevskiy <vsbrute@gmail.com>
2024-05-31 23:37:20 +0300

    chore: merge data in endpoints

--- src/pages/api/states/index.ts
+++
@@ -1,5 +0,0 @@
-// Use actual API version by default
-// But we still can use accurate API version
-// `/api/population/v0` and `/api/population` are both works as well
-
-export { default } from "./v0";
