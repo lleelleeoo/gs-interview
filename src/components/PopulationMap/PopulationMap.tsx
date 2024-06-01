9dabf2a3ebb397568a9bfbb9937559e44b5698ce
Vsevolod Drobyshevskiy <vsbrute@gmail.com>
2024-06-01 12:43:18 +0300

    feat: alive map component

--- src/components/PopulationMap/PopulationMap.tsx
+++ src/components/PopulationMap/PopulationMap.tsx
@@ -4,6 +4,7 @@ import "leaflet/dist/leaflet.css";
 import { Alert, CircularProgress } from "@mui/material";
 import axios from "axios";
 import { Feature,GeoJSON as GeoJSONType } from "geojson";
+import { Layer, LeafletMouseEvent } from "leaflet";
 import { useEffect, useState } from "react";
 import { GeoJSON, MapContainer, TileLayer } from "react-leaflet";
 
@@ -21,6 +22,9 @@ type PopulationMapProps = {
     showMapLayer?: boolean
 };
 
+// Tests here a little bit useless
+// Only external lib in use, no any data oerations
+// So I skipd it for this component
 export function PopulationMap({ position=[37.8, -96], zoom=3, showMapLayer=true }: PopulationMapProps) {
     const [isLoading, setLoading] = useState(false);
     const [isError, setError] = useState(false);
@@ -63,25 +67,61 @@ export function PopulationMap({ position=[37.8, -96], zoom=3, showMapLayer=true 
                     maxZoom={maxZoom}
                     minZoom={minZoom}
                 />}
-                {states && <GeoJSON data={states} style={getStyleForTile} />}
+                {states &&
+                    <GeoJSON
+                        data={states}
+                        style={getStyleForTile}
+                        onEachFeature={setStateInteractionFeatures}
+                    />
+                }
             </MapContainer>
+            <div className={styles.legend}>
+                {[6850000, 4500000, 2500000, 900000].map((el) => (
+                    <div key={el} className={styles["legend-item"]}>
+                        <div className={styles["legend-dot"]} style={{ background: getColor(el) }} />
+                        {`>${el/1000}k`}
+                    </div>
+                ))}
+            </div>
         </div>
 
     );
 }
 
 function getColor(p: number) {
-    return p < 900000 ? "#edf8fb" :
-        p < 2500000 ? "#ccece6" :
-            p < 4500000 ? "#99d8c9" :
-                p < 6850000 ? "#66c2a4" :
+    return p <= 900000 ? "#edf8fb" :
+        p <= 2500000 ? "#ccece6" :
+            p <= 4500000 ? "#99d8c9" :
+                p <= 6850000 ? "#66c2a4" :
                     "#2ca25f" ;
 }
 
 function getStyleForTile(f: Feature | undefined) {
     return {
+        color: "#00A757",
         fillColor: getColor(f?.properties?.population || 0),
         stroke: false,
         fillOpacity: 0.8
     };
+}
+
+function setStateInteractionFeatures(feature: Feature, layer: Layer) {
+    function highlightState({ target: state }: LeafletMouseEvent) {
+        state.setStyle({
+            stroke: true
+        });
+
+        state.bringToFront();
+    }
+
+    function resetHighlight({ target: state }: LeafletMouseEvent) {
+        state.setStyle({
+            stroke: false
+        });
+    }
+
+    layer.on({
+        mouseover: highlightState,
+        mouseout: resetHighlight,
+    });
 }
